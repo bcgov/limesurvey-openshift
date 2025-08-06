ac1a9b7e3264c2533da586c6468fb433a73130ea
NatSquared <nat.k.weiland@gmail.com>
2025-08-06 16:37:52 -0700

    Update container initialization

--- docker/php/limesurvey-init.sh
+++ docker/php/limesurvey-init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -e p
+set -e pipefail
 
-LIMESURVEY_DIR="/var/www/html/limesurvey"
-LOCKFILE="$LIMESURVEY_DIR/.limesurvey_downloaded.lock"
+EXTRACT_DIR="/var/www/html"
+LOCKFILE="$EXTRACT_DIR/.limesurvey_downloaded.lock"
 RELEASE="${LIMESURVEY_RELEASE:-6.15.5+250724.zip}"
 
 if [ ! -f "$LOCKFILE" ]; then
   echo "LimeSurvey not found, downloading..."
   curl -L "https://download.limesurvey.org/latest-master/limesurvey${RELEASE}" -o /tmp/limesurvey.zip
   echo "Download completed, extracting... (this may take a moment)"
-  unzip -o /tmp/limesurvey.zip -d "$LIMESURVEY_DIR" | pv -l -s $(unzip -l /tmp/limesurvey.zip | wc -l)
+  unzip -o /tmp/limesurvey.zip -d "$EXTRACT_DIR" | pv -l -s $(unzip -l /tmp/limesurvey.zip | wc -l)
   echo "Unzip completed, cleaning up..."
   rm -f /tmp/limesurvey.zip
   echo "Creating lock file to prevent re-download..."
