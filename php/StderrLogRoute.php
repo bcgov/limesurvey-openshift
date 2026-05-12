e2240d8a56636926bb8b3f7d241e60bd1bd9bf42
Helm <Uwe.Helm@gov.bc.ca>
2026-05-12 13:59:37 -0700

    feat: Custom logging route to send application logs to stderr

---
+++ php/StderrLogRoute.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * StderrLogRoute - Yii 1.x log route for container/Kubernetes environments.
+ *
+ * CFileLogRoute uses file_put_contents() with LOCK_EX, which fails on pipes
+ * and special file descriptors (/dev/stderr, php://stderr, /proc/1/fd/2).
+ * This class uses error_log() instead, which PHP-FPM routes through its own
+ * error log. The official PHP Docker image configures error_log = /dev/stderr,
+ * so output appears on the container's stderr and is captured by Kubernetes.
+ */
+class StderrLogRoute extends CLogRoute
+{
+    /**
+     * @param array $logs list of log messages, each an array of:
+     *   [message, level, category, timestamp]
+     */
+    protected function processLogs($logs)
+    {
+        foreach ($logs as $log) {
+            [$message, $level, $category, $timestamp] = $log;
+            $message = rtrim($message);
+            $entry = '[' . strtoupper($level) . '] [' . $category . '] ' . $message;
+            error_log($entry);
+        }
+    }
+}
